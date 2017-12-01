4d432963b2177302e9d8f4a52d6b82702e4452bf
yukimakura <ykazuki0806@gmail.com>
2017-12-01 13:40:47 +0900

    背景の色変更 ネムのアカウントステータスアクティビティの追加

---
+++ app/src/main/java/jp/yukimakura/monem/NemActivity.java
@@ -0,0 +1,18 @@
+package jp.yukimakura.monem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yukimakura on 2017/12/01.
+ */
+
+public class NemActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle nembundle){
+        super.onCreate(nembundle);
+
+
+    }
+}
+
